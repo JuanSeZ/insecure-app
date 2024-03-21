b3cc54a6e1182e99d7ff333318a75640a7eb4563
JuanSeZ <juan.zanelli@ing.austral.edu.ar>
2024-03-21 20:01:14 -0300

    xss tab

--- src/Tabs/XssTab.js
+++ src/Tabs/XssTab.js
@@ -9,25 +9,42 @@ export const XssTab = () => {
   const [customizedBackground, setCustomizedBackground] = useState('{ "style": { "backgroundColor": "red", "width": "100px", "height": "100px"}}')
   
   const [backgroundProps, setBackgroundProps] = useState({})
+
+  const allowedPlatforms = ['https://www.facebook.com/', 'https://www.instagram.com/', 'https://twitter.com/'];
+  const allowedProps = ['style'];
+
   const applyStyle = () => {
     
     try {
       const parsedJson = JSON.parse(customizedBackground);
-      setBackgroundProps(parsedJson);
+      const keys = Object.keys(parsedJson);
+      if (keys.every(key => allowedProps.includes(key))) {
+        setBackgroundProps(parsedJson);
+      } else {
+        setBackgroundProps(undefined);
+      }
     } catch (e) {
       console.error(e)
       setBackgroundProps(undefined)
     }
   }
 
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!allowedPlatforms.some(platform => value.startsWith(platform))) {
+      alert('Invalid social media URL. Only Facebook, Instagram, and Twitter are allowed.');
+    }
+  }
+
   return (
     <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
 
       <h1>Set your profile link</h1>
 
-      <Input style={{width: '90%'}} value={value} onChange={(e) => setValue(e.target.value)}/>
-      <a href={value} >Test profile link</a>
+      <form onSubmit={handleSubmit}>
+        <Input style={{width: '90%'}} value={value} onChange={(e) => setValue(e.target.value)}/>
+        <Button type="submit">Test profile link</Button>
+      </form>
 
       <h1>Customize your profile background</h1>
       <Input style={{width: '90%'}} value={customizedBackground} onChange={(e) => setCustomizedBackground(e.target.value)}/>
